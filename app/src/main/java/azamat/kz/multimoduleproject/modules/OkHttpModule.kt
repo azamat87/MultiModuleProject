7dae6b313cd0a04d74acd8248c0a18091b6f69ee
azamat87 <azamat87@gmail.com>
2020-06-05 20:07:01 +0600

    create network module

--- app/src/main/java/azamat/kz/multimoduleproject/modules/OkHttpModule.kt
+++ app/src/main/java/azamat/kz/multimoduleproject/modules/OkHttpModule.kt
@@ -1,2 +1,28 @@
 package azamat.kz.multimoduleproject.modules
 
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+private const val TIMEOUT_CONNECTION_SECONDS = 10L
+private const val TIMEOUT_IO_OPERATION_SECONDS = 10L
+
+@Module
+class OkHttpModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpBuilder(
+    ): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+            .followRedirects(true)
+            .connectTimeout(TIMEOUT_CONNECTION_SECONDS, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT_IO_OPERATION_SECONDS, TimeUnit.SECONDS)
+            .writeTimeout(TIMEOUT_IO_OPERATION_SECONDS, TimeUnit.SECONDS)
+            .protocols(listOf(Protocol.HTTP_1_1))
+        return builder.build()
+    }
+}
