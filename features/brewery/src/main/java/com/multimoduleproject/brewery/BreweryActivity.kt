02adb2fc0588f2ca0ed388e5d9dcdf087c51ce4e
azamat87 <azamat87@gmail.com>
2020-06-08 20:14:47 +0600

    create BreweryActivity

---
+++ features/brewery/src/main/java/com/multimoduleproject/brewery/BreweryActivity.kt
@@ -0,0 +1,15 @@
+package com.multimoduleproject.brewery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BreweryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_brewery)
+
+
+
+    }
+}
